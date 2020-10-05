43ea9834d8b2b70bbc5657c22155a1c21f76955a
hamzaanis <hamzaanis9514@gmail.com>
2020-10-06 00:32:36 +0500

    Updates

---
+++ precache-manifest.86fab47ae092543fd59bfa87bc809808.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb755e2819bc3ba41b203aafd0a8244",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "a6e4b26fe4c377a0b3f0",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c9a7122f1c95b4e60ed",
+    "url": "/test/static/js/2.c91f6db3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.c91f6db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6e4b26fe4c377a0b3f0",
+    "url": "/test/static/js/main.0213b5cc.chunk.js"
+  },
+  {
+    "revision": "9ac27bdfa2f6ca5dde0e",
+    "url": "/test/static/js/runtime-main.547695f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
